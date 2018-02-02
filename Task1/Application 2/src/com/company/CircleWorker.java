55575fe24d65fa917120930cfd786482439ef642
Ekaterina Shevyakova <ekaterinashevyako@gmail.com>
2018-02-02 19:52:30 +0300

    changed method names

--- Task1/Application 2/src/com/company/CircleWorker.java
+++ Task1/Application 2/src/com/company/CircleWorker.java
@@ -1,11 +1,11 @@
 package com.company;
 
 public class CircleWorker {
-    private static double circleSquareCalculating(double radius){
+    private static double calcuklateCircleSquare(double radius){
         return Math.PI * radius * radius;
     }
 
-    public static double squareOfRingCalculating(double outerCircleRadius, double innerCircleRadius){
+    public static double calculateSquareOfRing(double outerCircleRadius, double innerCircleRadius){
         double squareOuterCircle = CircleWorker.circleSquareCalculating(outerCircleRadius);
         double squareInnerCircle = CircleWorker.circleSquareCalculating(innerCircleRadius);
         return squareOuterCircle - squareInnerCircle;
--- Task1/Application 4/src/com/company/Checker.java
+++ Task1/Application 4/src/com/company/Checker.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Checker {
 
-    public static boolean IsIncreasing(int number) {
+    public static boolean isIncreasing(int number) {
         int a = number % 10;
         int b = (number / 10) % 10;
         int c = (number / 100) % 10;
